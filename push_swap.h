6c067679c6a7080f03d80cdc81bfd03d10b93065
Marta Diez Asensio <mdiez-as@cbr1s1.42barcelona.com>
2023-06-10 15:28:50 +0200

    Adition of files

---
+++ push_swap.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mdiez-as <mdiez-as@student.42barcelona.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/10 15:15:15 by mdiez-as          #+#    #+#             */
+/*   Updated: 2023/06/10 15:28:20 by mdiez-as         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef	PUSH_SHWAP_H
+# define PUSH_SWAP_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+//Stack structure
+
+
+#endif
